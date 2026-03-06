8af8dc539f7c3d5dd92c3652819484fe253da328
Luiz Carlos Pielak <pielak.ctba@gmail.com>
2026-03-06 13:40:33 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -30,3 +30,5 @@ def atualizar(id: int, cliente: Cliente):
 def deletar(id: int):
     if not database.delete(id):
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
+
+# teste de commit
